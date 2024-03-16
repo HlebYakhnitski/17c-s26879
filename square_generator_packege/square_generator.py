1889d42f78b6e373af04f81488948b727af476e7
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:23:24 +0100

    eights task commit

--- square_generator_packege/square_generator.py
+++ square_generator_packege/square_generator.py
@@ -1,7 +1,16 @@
-import math
-
 class SquareGenerator:
     def generate_squares(self, start, end):
         if end < start:
             return []
         return [x**2 for x in range(start, end + 1)]
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+        if end < start:
+            return []
+        return [x**3 for x in range(start, end + 1)]
+
+cubic_gen = CubicGenerator()
+
+cubes_list = cubic_gen.generate_cubes(1, 5)
+print(cubes_list)
