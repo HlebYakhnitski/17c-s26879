2100cf2f552342897bb5c750a80626960b816429
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:24:45 +0100

    ninth task commit

--- square_generator_packege/square_generator.py
+++ square_generator_packege/square_generator.py
@@ -5,12 +5,12 @@ class SquareGenerator:
         return [x**2 for x in range(start, end + 1)]
 
 class CubicGenerator(SquareGenerator):
-    def generate_cubes(self, start, end):
+    def generate_squares(self, start, end):
         if end < start:
-            return []
-        return [x**3 for x in range(start, end + 1)]
+            raise ValueError("End of range must be greater than or equal to start.")
+        return [x**2 for x in range(start, end + 1)]
 
 cubic_gen = CubicGenerator()
 
-cubes_list = cubic_gen.generate_cubes(1, 5)
-print(cubes_list)
+squares_list = cubic_gen.generate_squares(1, 5)
+print(squares_list)
