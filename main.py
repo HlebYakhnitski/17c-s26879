41bb3a95bdcb70201ccd2419692d383c47752707
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:04:43 +0100

    second task commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
 
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+example_squares = generate_squares(1, 10)
+print(example_squares)
