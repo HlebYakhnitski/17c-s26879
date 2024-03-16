8861d41f02740188f938376b97c56f522f3faf06
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:05:10 +0100

    third task commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def generate_squares(start, end):
-    return [x**2 for x in range(start, end + 1)]
 
-example_squares = generate_squares(1, 10)
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end + 1)]
+
+square_gen = SquareGenerator()
+example_squares = square_gen.generate_squares(1, 10)
 print(example_squares)
