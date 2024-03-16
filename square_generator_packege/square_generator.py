56d4bcb54e044776a6a9bb281139ca69772c776b
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:28:33 +0100

    tens task commit

--- square_generator_packege/square_generator.py
+++ square_generator_packege/square_generator.py
@@ -1,16 +1,6 @@
-class SquareGenerator:
+from abc import ABC, abstractmethod
+
+class SquareGenerator(ABC):
+    @abstractmethod
     def generate_squares(self, start, end):
-        if end < start:
-            return []
-        return [x**2 for x in range(start, end + 1)]
-
-class CubicGenerator(SquareGenerator):
-    def generate_squares(self, start, end):
-        if end < start:
-            raise ValueError("End of range must be greater than or equal to start.")
-        return [x**2 for x in range(start, end + 1)]
-
-cubic_gen = CubicGenerator()
-
-squares_list = cubic_gen.generate_squares(1, 5)
-print(squares_list)
+        pass
