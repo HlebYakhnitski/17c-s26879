76569e9dbd8e2c98c60446f60d5542318afd9695
s26879 <s26879pjwstk.edu.pl>
2024-03-16 20:06:10 +0100

    forth task commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
+import math
+
 class SquareGenerator:
     def generate_squares(self, start, end):
+
         return [x**2 for x in range(start, end + 1)]
 
 square_gen = SquareGenerator()
-example_squares = square_gen.generate_squares(1, 10)
-print(example_squares)
+
+squares_list = square_gen.generate_squares(1, 10)
+
+square_roots = [math.sqrt(number) for number in squares_list]
+
+print("Squares list:", squares_list)
+
+print("Square roots list:", square_roots)
